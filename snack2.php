9dc51767593dfa6cdc776f6c17dc0d2211dd9366
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-17 16:14:24 +0200

    add logic for mail password and age control

---
+++ snack2.php
@@ -0,0 +1,34 @@
+<?php
+$user_name = $_GET['user_name'];
+$user_age = $_GET['user_age'];
+$user_email = $_GET['user_email'];
+
+$message = '';
+
+if (
+    $user_name >= 3 &&
+    (strpos($user_email, '@') !== false && strpos($user_email, '.') !== false) &&
+    is_numeric($user_age)
+) {
+    $message = 'Accesso abilitato';
+} else {
+    $message = 'accesso negato';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <h2>
+        <?= $message ?>
+    </h2>
+</body>
+
+</html>
