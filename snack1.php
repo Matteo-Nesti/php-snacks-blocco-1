4581023df46b1397f462b65241857a0198e29af4
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-17 15:24:45 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,22 @@
+<?php
+$games = [
+    [
+        'home_team' => 'milano',
+        'away_team' => 'napoli',
+        'home_score' => '3',
+        'home_score' => '1'
+    ],
+    [
+        'home_team' => 'inter',
+        'away_team' => 'fiorentina',
+        'home_score' => '1',
+        'home_score' => '1'
+    ],
+    [
+        'home_team' => 'verona',
+        'away_team' => 'genoa',
+        'home_score' => '1',
+        'home_score' => '2'
+    ],
+
+];
